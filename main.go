813d68fad8bd8cf7cc42296061bcef0bbbf0e51d
Benjamin Trollope <benjamin.trollope@deliveryhero.com>
2021-05-23 22:24:29 +0200

    adding fibre + hello world init

--- main.go
+++ main.go
@@ -1,2 +1,15 @@
 // setup tutorial https://youtu.be/d4Y2DkKbxM0?t=199
+//go run main.go 
 package main
+
+import "github.com/gofiber/fiber"
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World 👋!")
+	})
+
+	app.Listen(":8000")
+}
